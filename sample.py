c0ca9a79d5aedfb8c31fc1ade881a61c7107a544
Arnar Ingason <arnari@Arnars-MacBook-Pro.local>
2022-03-02 20:55:32 +0000

    gerði test sem athugar hvort talan sé hvorki deilanleg með 3 né 5(skilar þá sömu tölu)  og fékk það svo til að virka

--- sample.py
+++ sample.py
@@ -1,9 +1,11 @@
 def FizzBuzz(int):
     if int%3==0 and int%5==0:
         return "FizzBuzz"
-        
+
     if int%5==0:
         return "Buzz"
 
     if int%3==0:
         return "Fuzz"
+    
+    return int
--- test_sample.py
+++ test_sample.py
@@ -8,3 +8,6 @@ def test_is_divisible_by_5():
 
 def test_is_divisible_by_3():
     assert FizzBuzz(3) == "Fuzz"
+
+def test_if_neither_divisible_by_5_nor_3():
+    assert FizzBuzz(11) == 11
