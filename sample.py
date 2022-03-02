3f8c6cc6f79d7029a1d45491d970c7bba47885b0
Arnar Ingason <arnari@Arnars-MacBook-Pro.local>
2022-03-02 20:42:49 +0000

    gerði test sem athugar hvort talan sé deilanleg með 5 og fékk það svo til að virka

--- sample.py
+++ sample.py
@@ -1,3 +1,5 @@
 def FizzBuzz(int):
     if int%3==0 and int%5==0:
         return "FizzBuzz"
+    if int%5==0:
+        return "Buzz"
--- test_sample.py
+++ test_sample.py
@@ -2,3 +2,6 @@ from sample import FizzBuzz
 
 def test_is_divisible_by_5_and_3():
     assert FizzBuzz(15) == "FizzBuzz"
+
+def test_is_divisible_by_5():
+    assert FizzBuzz(5) == "Buzz"
